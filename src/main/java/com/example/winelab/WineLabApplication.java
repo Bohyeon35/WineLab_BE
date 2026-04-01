85c72c4eeb8084f20d53be76c4351a973b86255f
leebohyeon <lilddy35@gmail.com>
2026-04-01 14:19:05 +0900

    one

---
+++ src/main/java/com/example/winelab/WineLabApplication.java
@@ -0,0 +1,13 @@
+package com.example.winelab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WineLabApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/winelab/WineLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.winelab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
