5ae403cbbb7fe9add33a8845c1c65eadbde2e1a1
i1000u <grandromantic77@gmail.com>
2026-07-06 19:56:06 +0900

    배포 및 테스트 환경 설정 정리

--- src/test/java/com/example/winelab/WineLabApplicationTests.java
+++ src/test/java/com/example/winelab/WineLabApplicationTests.java
@@ -2,7 +2,9 @@ package com.example.winelab;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class WineLabApplicationTests {
 
@@ -11,3 +13,4 @@ class WineLabApplicationTests {
     }
 
 }
+
